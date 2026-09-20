11e4efd3ad0e975b26de80263eb3b6034650e6e2
VAIXLNS <259728808+fisallllll280-code@users.noreply.github.com>
2026-09-20 05:15:04 +0300

    feat: expose engineering coordination ontology and contradiction layer

--- nexent/engineering/__init__.py
+++ nexent/engineering/__init__.py
@@ -1,4 +1,6 @@
 from .core import Artifact, EngineeringSystem, SystemContract, VerificationStatus
 from .runtime import EngineeringRuntime, RuntimeResult
 from .ideas import EngineeringHypothesis
-__all__=["Artifact","EngineeringSystem","SystemContract","VerificationStatus","EngineeringRuntime","RuntimeResult","EngineeringHypothesis"]
+from .coordination import EngineeringTask, CoordinationResult, MultiMindCoordinator
+from .contradiction import Conflict, ConflictKind, ContradictionEngine
+from .neo import NEORecord, NEXENTEngineeringOntology
